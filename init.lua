74ff45d081838419e52e9cbb64b8379e7579e4e2
luk3yx <luk3yx@users.noreply.github.com>
2022-10-09 20:15:54 +1300

    Bugfix

--- init.lua
+++ init.lua
@@ -153,7 +153,7 @@ end
 
 local invisible_elems = {
     style = true, listring = true, scrollbaroptions = true, tableoptions = true,
-    tablecolumns = true, tooltip = true,
+    tablecolumns = true, tooltip = true, style_type = true,
 }
 
 local DEFAULT_SPACING = 0.2
