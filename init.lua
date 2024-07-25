d14e430a74c63b2f5cd7ee8ecabf1ac6ea57540a
luk3yx <luk3yx@users.noreply.github.com>
2024-07-25 19:39:32 +1200

    Rename to embed.lua

--- init.lua
+++ init.lua
@@ -1230,7 +1230,7 @@ function Form:update_where(func)
     end
 end
 
-Form.embed = assert(loadfile(modpath .. "/embeded.lua"))(function(new_context)
+Form.embed = assert(loadfile(modpath .. "/embed.lua"))(function(new_context)
     current_ctx = new_context
 end)
 
