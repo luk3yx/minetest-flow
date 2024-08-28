e0626bdb421a6adca34cc4b09d22186c730c63f1
luk3yx <luk3yx@users.noreply.github.com>
2024-08-28 17:47:06 +1200

    Relicense to LGPL 2.1+
    I think it's unlikely that flow will be used in the main menu, but now
    licensing (hopefully) won't be an issue

--- example.lua
+++ example.lua
@@ -1,3 +1,22 @@
+--
+-- Minetest formspec layout engine
+--
+-- Copyright © 2022 by luk3yx
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation, either version 2.1 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--
+
 -- You can run /flow-example in singleplayer to open this form
 local gui = flow.widgets
 local S = minetest.get_translator("flow")
